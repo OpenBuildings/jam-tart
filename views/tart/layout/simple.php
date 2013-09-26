f19f0b4f38c1954a37e89c972e74d5aaa822a31d
Haralan Dobrev <hkdobrev@gmail.com>
2013-09-26 13:09:45 +0300

    Load correct resources for simple layout

--- views/tart/layout/simple.php
+++ views/tart/layout/simple.php
@@ -4,8 +4,8 @@
 		<title><?php echo isset($title) ? $title : 'Admin' ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="/jam-tart/css/bootstrap.min.css" rel="stylesheet" media="screen">
-		<link href="/jam-tart/css/jam-tart.css" rel="stylesheet" media="screen">
 		<link href="/jam-tart/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+		<link href="/jam-tart/css/general.css" rel="stylesheet" media="all">
 	</head>
 	<body>
 		<?php echo Tart_Html::navigation(array(Tart::uri('session', 'new') => 'Login')) ?>
@@ -17,7 +17,7 @@
 		</div>
 		
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-		<script src="/jam-tart/js/underscore-min.js"></script>
-		<script src="/jam-tart/js/bootstrap.js"></script>
+		<script src="/jam-tart/js/plugins.min.js"></script>
+		<script src="/jam-tart/js/bootstrap.min.js"></script>
 	</body>
 </html>
