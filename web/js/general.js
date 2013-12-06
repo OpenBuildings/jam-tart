8726834ab83fbfaf5d75748aaca0c7e8fea2be88
Yasen Yanev <yasen@openbuildings.com>
2013-12-06 18:44:31 +0200

    Dropdown menus fix in admin for ipad

--- web/js/general.js
+++ web/js/general.js
@@ -102,4 +102,13 @@ $(function(){
 			}
 		});
 	});
+
+	$('.dropdown-toggle').click(function(e) {
+	  e.preventDefault();
+	  setTimeout($.proxy(function() {
+	    if ('ontouchstart' in document.documentElement) {
+	      $(this).siblings('.dropdown-backdrop').off().remove();
+	    }
+	  }, this), 0);
+	});
 });
