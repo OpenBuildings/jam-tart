d8dfa7d42f174eca8027276b23ccc35571ae5ab3
Haralan Dobrev <hkdobrev@gmail.com>
2013-12-12 15:56:34 +0200

    Reindent and check for item in remoteselect.js
    Previous commits have broken the indentation in remoteselect.js
    
    It turns out in remoteselect widget add method could be called with `null` as first argument.

--- web/js/bootstrap-extensions.js
+++ web/js/bootstrap-extensions.js
@@ -530,140 +530,137 @@ $(function () {
 
 }(window.jQuery);!function ($) {
 
-    "use strict"; // jshint ;_;
+"use strict"; // jshint ;_;
 
 
- /* REMOTESELECT CLASS DEFINITION
-    * ==================== */
+/* REMOTESELECT CLASS DEFINITION
+* ==================== */
 
-    var Remoteselect = function (element, options) {
-	var $element = this.$element = $(element);
-	this.url = options.url;
-	this.templatestring = options.templatestring;
-	this.count = options.count || $(options.container).children().length;
-	this.$container = $(options.container);
-	this.$element
-	    .typeahead({
+var Remoteselect = function (element, options) {
+var $element = this.$element = $(element);
+this.url = options.url;
+this.templatestring = options.templatestring;
+this.count = options.count || $(options.container).children().length;
+this.$container = $(options.container);
+this.$element
+	.typeahead({
 		source: options.source,
 		val: {},
 		itemSelected: function(item){
-		    if (options.overwrite) {
-			$(options.container).empty();
-			$element.addClass('hide');
-		    }
-		    $element
-			.val('')
-			.remoteselect('add', item);
+			if (options.overwrite) {
+				$(options.container).empty();
+				$element.addClass('hide');
+			}
+			$element
+				.val('')
+				.remoteselect('add', item);
 		}
-	    });
-    };
+	});
+};
 
-    Remoteselect.prototype = {
+Remoteselect.prototype = {
 
 	constructor: Remoteselect,
 
 	add: function(item)
 	{
-	    var $container = this.$container;
+		var $container = this.$container;
 
-	    if (this.templatestring) {
-		$container.append(
-		    this.templatestring
-			.replace(/\{\{id\}\}/g, item.id)
-			.replace(/\{\{variations\}\}/g, item.variations)
-			.replace(/\{\{count\}\}/g, this.count++)
-			.replace(/\{\{model\}\}/g, item.model)
-			.replace(/\{\{name\}\}/g, item.name)
-			.replace(/\{\{price\}\}/g, item.price)
-			.replace(/\{\{url\}\}/g, item.url)
-		);
-		$(".chzn-select").chosen();
-	    } else {
-		$.get(
-		    this.url
-			.replace(/\{\{id\}\}/g, item.id)
-			.replace(/\{\{count\}\}/g, this.count++)
-			.replace(/\{\{model\}\}/g, item.model),
-		    function(data){
-			$container.append(data);
+		if (this.templatestring) {
+			$container.append(
+				this.templatestring
+				.replace(/\{\{id\}\}/g, item.id)
+				.replace(/\{\{variations\}\}/g, item.variations)
+				.replace(/\{\{count\}\}/g, this.count++)
+				.replace(/\{\{model\}\}/g, item.model)
+				.replace(/\{\{name\}\}/g, item.name)
+				.replace(/\{\{price\}\}/g, item.price)
+				.replace(/\{\{url\}\}/g, item.url)
+			);
 			$(".chzn-select").chosen();
-		    }
-		);
-	    }
+		} else if (item) {
+			$.get(
+				this.url
+					.replace(/\{\{id\}\}/g, item.id)
+					.replace(/\{\{count\}\}/g, this.count++)
+					.replace(/\{\{model\}\}/g, item.model),
+				function(data){
+					$container.append(data);
+					$(".chzn-select").chosen();
+				}
+			);
+		}
 	}
-    };
+};
 
 
- /* REMOTESELECT PLUGIN DEFINITION
-    * ===================== */
+/* REMOTESELECT PLUGIN DEFINITION
+* ===================== */
 
-    var old = $.fn.remoteselect;
+var old = $.fn.remoteselect;
 
-    $.fn.remoteselect = function (option, param, param2) {
+$.fn.remoteselect = function (option, param, param2) {
 	return this.each(function () {
-	    var $this = $(this),
-		    data = $this.data('remoteselect'),
-		    options = typeof option === 'object' && option;
+		var $this = $(this),
+			data = $this.data('remoteselect'),
+			options = typeof option === 'object' && option;
 
-	    if (!data) {
-		$this.data('remoteselect', (data = new Remoteselect(this, options || $this.data())));
-	    }
+		if (!data) {
+			$this.data('remoteselect', (data = new Remoteselect(this, options || $this.data())));
+		}
 
-	    if (typeof option === 'string') {
-		data[option](param, param2);
-	    }
+		if (typeof option === 'string') {
+			data[option](param, param2);
+		}
 	});
-    };
+};
 
-    $.fn.remoteselect.Constructor = Remoteselect;
+$.fn.remoteselect.Constructor = Remoteselect;
 
 
- /* REMOTESELECT NO CONFLICT
-    * =============== */
+/* REMOTESELECT NO CONFLICT
+* =============== */
 
-    $.fn.remoteselect.noConflict = function () {
+$.fn.remoteselect.noConflict = function () {
 	$.fn.remoteselect = old;
 	return this;
-    };
+};
 
 
- /* REMOTESELECT DATA-API
-    * ============ */
+/* REMOTESELECT DATA-API
+* ============ */
 
-    $(function () {
+$(function () {
 	$(document).on('focus.remoteselect.data-api', '[data-provide="remoteselect"]', function (e) {
-	    var $this = $(this);
-	    if ($this.data('remoteselect')) return;
-	    e.preventDefault();
-	    $this.remoteselect($this.data());
+		var $this = $(this);
+		if ($this.data('remoteselect')) return;
+		e.preventDefault();
+		$this.remoteselect($this.data());
 	});
 
 	$(document).on('click.remoteselect.data-api', '[data-dismiss="remoteselect"]', function (e) {
-	    var item = $(this).closest('.remoteselect-item');
+		var item = $(this).closest('.remoteselect-item');
 
-	    e.preventDefault();
+		e.preventDefault();
 
-	    function removeItem()
-	    {
-		$('input[data-container="#'+item.parent().attr('id')+'"]').removeClass('hide').addClass('in');
-		item.remove();
-	    }
+		function removeItem() {
+			$('input[data-container="#'+item.parent().attr('id')+'"]').removeClass('hide').addClass('in');
+			item.remove();
+		}
 
-	    if ($.support.transition) {
-		item.addClass('fade').on($.support.transition.end, removeItem);
-	    }
-	    else
-	    {
-		removeItem();
-	    }
+		if ($.support.transition) {
+			item.addClass('fade').on($.support.transition.end, removeItem);
+		} else {
+			removeItem();
+		}
 	});
 
 	$(document).on('click.remoteselect.data-api', '[data-remoteselect-new]', function (e) {
-	    var $this = $($(this).attr('href')).length ? $($(this).attr('href')) : $(this).siblings('[data-provide="remoteselect"]');
-	    e.preventDefault();
-	    $this.remoteselect('add', null, $(this).data('remoteselectNew'));
+		var $this = $($(this).attr('href')).length ? $($(this).attr('href')) : $(this).siblings('[data-provide="remoteselect"]');
+		e.preventDefault();
+		$this.remoteselect('add', null, $(this).data('remoteselectNew'));
 	});
-    });
+});
 
 }(window.jQuery);!function ($) {
 
