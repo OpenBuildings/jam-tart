11da80efe38f5b6aba041aca7eaec447b3099e31
Yasen Yanev <yasen@openbuildings.com>
2014-03-21 12:41:20 +0200

    Fixed unclosed tag

--- classes/Kohana/Tart/Column.php
+++ classes/Kohana/Tart/Column.php
@@ -141,7 +141,7 @@ abstract class Kohana_Tart_Column extends Tart_Group_Item {
 
 		return
 			HTML::anchor(Tart::uri($this->controller()).URL::query(array('sort' => $this->name().':'.$direction)),
-			$this->label().' '.'<i class="'.$class.'"><i>');
+			$this->label().' '.'<i class="'.$class.'"></i>');
 	}
 
 	protected static function render_association(Jam_Model $item, Jam_Association $association)
